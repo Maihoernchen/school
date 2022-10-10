22cfd801090e22ebeab69d8ee3960e53024d1d31
SkeL <maihoernchen.index@gmail.com>
2022-10-10 12:55:43 +0200

    duck the zucc

---
+++ info/HtmlElement.py
@@ -0,0 +1,15 @@
+from msilib.schema import SelfReg
+
+
+class TextElement:
+    __text = ""
+    def __init__(self,text):
+        self.__text = text
+    def render(self):
+        return self.__text
+class HtmlElement(TextElement):
+        _name = ""
+        def __init__(self, name):
+            self._name = name
+        def render(self):
+            return "<"+self._name+">"
