8cf65e2bb93453a6961fc9180fbaa4ae47a72dcb
SkeL <maihoernchen.index@gmail.com>
2022-11-07 12:08:55 +0100

    duck the zucc

--- info/HtmlList.php
+++ info/HtmlList.php
@@ -8,6 +8,12 @@
             $this->name = $name;
             $this->list = $list;
         }
+        function sortUp(){
+            sort($this->list);
+        }
+        function sortDown(){
+            rsort($this->list);
+        }
         function setListType($ordered){
             $this->ordered = $ordered;
         }
@@ -19,6 +25,16 @@
                 return renderUL();
             } else {
                 return renderOL();
+            }
+        }
+        function search($obj){
+            return array_search($obj,$this->list);
+        }
+        function rendercsv($sep=','){
+            $csv;
+            foreach($this->list as $x => $x_value) {
+                $csv .= $x_value;
+                $csv .= $sep;
             }
         }
         function renderUL(){
